aad6143d98729108234c1367303bef3bc5dd2b03
Rodrigo Fernando Jiménez Dencker <rjimenez@pasiona.com>
2019-11-04 13:43:27 +0100

    Añadidos every,some,find,filter,foreach,map,reduce,indexof...

--- login_webstorage/script.js
+++ login_webstorage/script.js
@@ -1,4 +1,5 @@
 let tareas = [];
+iniciar();
 document.getElementById("boton_guardar").addEventListener("click", guardarDatos);
 
 function guardarDatos(){
@@ -19,30 +20,41 @@ function guardarDatos(){
 function mostrarLista(){
     document.getElementById("lista").innerHTML = "";
     document.getElementById("lista").innerHTML = 
-    "<tr>" +
+    "<thead><tr>" +
         "<th>Tarea</th>" +
         "<th>Priodidad</th>" +
         "<th>Fecha</th>" +
         "<th>Acción</th>" +
-    "</tr>";
+    "</tr></thead>" +
+    "<tbody id='body_table'></tbody>";
     for (let i = 0; i < tareas.length; i++) {
-        document.getElementById("lista").innerHTML += 
+        document.getElementById("body_table").innerHTML += 
         "<tr><td>" + tareas[i].tarea + "</td>" + 
         "<td>" + tareas[i].prioridad + "</td>" + 
         "<td>" + tareas[i].fecha + "</td>" +
-        "<td><button id='" + i + "' class='btn btn-danger'>Borrar</button></tr>";
-        document.getElementById(i).addEventListener("click",() => eliminarTarea(this.i));
+        "<td><button id='" + i + "' class='boton_borrar btn btn-danger'>Borrar</button></tr>";
+    }
+    let botones = document.getElementsByClassName("boton_borrar");
+    for (let i = 0; i < botones.length; i++){
+        botones[i].addEventListener("click", () => eliminarTarea(i));
     }
     setLocalStorage();
 }
 
 function eliminarTarea(id){
+    console.log(id);
      tareas.splice(id, 1); 
      mostrarLista();
      setLocalStorage();
 }
 
-
 function setLocalStorage() {
     localStorage.array = JSON.stringify(tareas);
+}
+
+function iniciar() {
+    if (localStorage.array) {
+        tareas = JSON.parse(localStorage.array);
+    }
+    mostrarLista();
 }
