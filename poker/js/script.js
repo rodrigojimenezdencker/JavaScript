b2d6fa27b99a8999746c546c9ce0afaf3f09adbc
Rodrigo Fernando Jiménez Dencker <rjimenez@pasiona.com>
2019-11-10 22:54:22 +0100

    Poker finalizado, todos los fetch van bien

--- poker/js/script.js
+++ poker/js/script.js
@@ -1,15 +1,55 @@
-document.getElementById('shuffle_button').addEventListener('click', shuffleDecks);
+document.getElementById('shuffle_button').addEventListener('click', startGame);
 
+function startGame() {
+    generateRandomPlayers();
+    generateRobotPlayerImage();
+    generateAvatarPlayerImage();
+    shuffleDecks();
+}
 
 function shuffleDecks() {
-    fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=5')
-        .then(reponse => reponse.json())
+    fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+        .then(response => response.json())
         .then(data => {
-            for (let card of data.cards) {
-                let template_decks = document.querySelector('[data-hook="template_decks"]').content;
-                let deck = document.importNode(template_decks, true);
-                deck.querySelector('[data-hook="card"]').src = card.image;
-                document.querySelector('[data-hook="deck"]').appendChild(deck);
-            }
+            document.querySelectorAll('[data-hook="player_deck"]').forEach(player_deck =>
+                fetch('https://deckofcardsapi.com/api/deck/' + data.deck_id + '/draw/?count=5')
+                    .then(reponse => reponse.json())
+                    .then(data => {
+                        player_deck.innerHTML = "";
+                        for (let card_value of data.cards) {
+                            let template_card = document.querySelector('[data-hook="template_card"]').content;
+                            let card = document.importNode(template_card, true);
+                            card.querySelector('[data-hook="card"]').src = card_value.image;
+                            player_deck.appendChild(card);
+                        }
+                    })
+            );
         });
+}
+
+function generateRandomPlayers() {
+    document.querySelectorAll('[data-hook="random_player_information"]').forEach(element => {
+        fetch('https://randomuser.me/api/?results=1')
+            .then(response => response.json())
+            .then(data => {
+                data.results.forEach(e => {
+                    element.querySelector('[data-hook="random_player_name"]').textContent = e.name.first,
+                        element.querySelector('[data-hook="random_player_image"]').src = e.picture.large
+                });
+            });
+    });
+}
+
+function generateRobotPlayerImage() {
+    let randomNumber = Math.random();
+    fetch('https://robohash.org/' + randomNumber)
+        .then(response => response)
+        .then(data => document.querySelector('[data-hook="robot_player_image"]').src = data.url);
+}
+
+function generateAvatarPlayerImage() {
+    let randomNumber = Math.random();
+        fetch('https://api.adorable.io/avatars/285/' + randomNumber)
+            .then(response => response)
+            .then(data => document.querySelector('[data-hook="real_player_image"]').src = data.url);
 }
