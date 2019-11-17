cff0da418ffb14742d50cee88676774f3af29581
Rodrigo Fernando Jiménez Dencker <rjimenez@pasiona.com>
2019-11-17 13:44:09 +0100

    Falta poder editar

--- webapi_alumnos/js/alumnos.js
+++ webapi_alumnos/js/alumnos.js
@@ -65,13 +65,3 @@ if (document.getElementById("button_new_student") != null) {
 if (document.URL.includes("index.html")) {
     getStudents();
 }
-
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
